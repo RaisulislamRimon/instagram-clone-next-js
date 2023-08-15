d6c2d30b85bee06640350e1e9e7051e412f07013
MD. Raisul Islam Rimon <rimonppip0@gmail.com>
2023-08-15 22:24:45 +0600

    added logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,15 +4,26 @@ export default function Header(){
   return (
     <div>
       {/* left */}
-      <div className="">
-        <div className="">
+      <div className="flex items-center justify-between max-w-6xl mx-auto">
+        <div className="h-24 w-24 relative hidden lg:inline-grid cursor-pointer">
           <Image 
             src="https://i.ibb.co/SPLh928/images.png" 
             alt="instagram_logo" 
             layout="fill"
             className="object-contain"
           />
-        </div>        
+        </div>   
+        <div className="h-24 w-10 relative lg:hidden cursor-pointer">
+          <Image 
+            src="https://i.ibb.co/5MQkb64/2227.jpg" 
+            alt="instagram_logo" 
+            layout="fill"
+            className="object-contain"
+          />
+        </div>   
+        <h1>
+          Right side
+        </h1>           
       </div>
       {/* center */}
       {/* right */}
