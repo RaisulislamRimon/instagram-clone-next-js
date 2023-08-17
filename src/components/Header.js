c82bbd797fc71fafbbfdca9c0003706aced0195e
MD. Raisul Islam Rimon <rimonppip0@gmail.com>
2023-08-17 23:43:00 +0600

    add search-bar in header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -21,8 +21,17 @@ export default function Header(){
           className="object-contain"
         />
       </div> 
+      
       {/* center */}
-      <BeakerIcon className="h-6 w-6 text-blue-500" />
+      <div className="relative mt-1">
+        <div className="absolute top-2 left-2">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="h-5 text-gray-500">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
+          </svg>
+        </div>
+        <input type="text" placeholder="Search" className="bg-gray-50 pl-10 border-gray-500 text-sm focus:ring-black focus:border-black rounded-md" />
+      </div>
+      
       {/* right */}  
       <h1>
         Right side
